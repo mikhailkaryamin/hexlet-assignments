f06a9f3e2145ab659651e3a509f343e28769dfcf
@hexlet/cli <support@hexlet.io>
2024-09-29 15:26:18 +0300

    submit rails-basics-ru/crud

--- rails-basics-ru/crud/app/javascript/controllers/index.js
+++ rails-basics-ru/crud/app/javascript/controllers/index.js
@@ -1,6 +1,2 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
-window.Stimulus = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-Stimulus.load(definitionsFromContext(context))
